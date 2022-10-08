3dc4d78d4d7aa0afbedd4ad719b544c792db82e9
dorelysm <martinezdorelys@gmail.com>
2022-10-07 22:27:33 -0500

    correcciones

--- lib/provider/Pokemon_provider.dart
+++ lib/provider/Pokemon_provider.dart
@@ -4,15 +4,33 @@ import 'package:http/http.dart' as http;
 import 'package:pokeapp/models/Model_pokemons.dart';
 
 class PokemonProvider {
-  final url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
+  final url = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
+  final List pokemonList = [];
 
   Future<List<ModelPokemons>> getPokemons() async {
     final resp = await http.get(Uri.parse(url));
     if (resp.statusCode == 200) {
       String body = utf8.decode(resp.bodyBytes);
       final jsonData = jsonDecode(body);
+
+      for (var pokemon in jsonData['results']) {
+        var item = await getPokemon(pokemon['url']);
+        pokemonList.add(item);
+      }
+
       final pokemons = Pokemons.fromJsonList(jsonData);
       return pokemons.items;
+    } else {
+      throw Exception("Ocurrio Algo ${resp.statusCode}");
+    }
+  }
+
+  Future getPokemon(String url) async {
+    final resp = await http.get(Uri.parse(url));
+    if (resp.statusCode == 200) {
+      String body = utf8.decode(resp.bodyBytes);
+      final jsonData = jsonDecode(body);
+      return jsonData;
     } else {
       throw Exception("Ocurrio Algo ${resp.statusCode}");
     }
--- lib/widgets/listsPokemons.dart
+++ lib/widgets/listsPokemons.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:pokeapp/models/Model_pokemons.dart';
 import 'package:pokeapp/widgets/Pages.dart';
 
-List<Widget> listPokemonsPageView(List<ModelPokemons> data,
-    {required ModelPokemons ModelPokemons}) {
+Future<List<Widget>> listPokemonsPageView(List<ModelPokemons> data,
+    {required ModelPokemons ModelPokemons}) async {
   List<Widget> pokemons = [];
   for (var pokemon in data) {
     pokemons.add(Center(child: Pages(text: pokemon.name, imagen: pokemon.url)));
