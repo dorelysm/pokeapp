a4ae81d382e530848988f160815c9f8b0b5f0be1
dorelysm <martinezdorelys@gmail.com>
2022-10-24 10:55:09 -0500

    correcion model

--- lib/models/Model_pokemons.dart
+++ lib/models/Model_pokemons.dart
@@ -1,12 +1,10 @@
-import 'dart:convert';
-
 class Pokemons {
   List<ModelPokemons> items = [];
 
   Pokemons();
   Pokemons.fromJsonList(jsonList) {
     if (jsonList == null) return;
-    for (var item in jsonList['data']) {
+    for (var item in jsonList['results']) {
       final pokemon = new ModelPokemons.fromJsonMap(item);
       items.add(pokemon);
     }
